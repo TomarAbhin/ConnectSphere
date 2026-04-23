8dd9d813e738441c37211dd51b5af6ef2392c7b5
TomarAbhin <122555471+TomarAbhin@users.noreply.github.com>
2026-04-23 11:56:51 +0530

    [Abhinav Tomar] Refactored the code.

--- media-service/src/main/java/com/connectsphere/media/MediaServiceApplication.java
+++ media-service/src/main/java/com/connectsphere/media/MediaServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class jMediaServiceApplication {
+public class MediaServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MediaServiceApplication.class, args);
     }
